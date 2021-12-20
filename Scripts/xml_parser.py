609ba65d109eea0f8b1cf02fe49b10005c349596
Mislav Dominovic <mislav.dominovic@tttech-auto.com>
2021-12-20 15:29:23 +0100

    Add xml_parser.py

---
+++ Scripts/xml_parser.py
@@ -0,0 +1,37 @@
+class XmlList(list):
+    def __init__(self, aList):
+        for element in aList:
+            if element:
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    self.append(XmlDict(element))
+                elif element[0].tag == element[1].tag:
+                    self.append(XmlList(element))
+            elif element.text:
+                text = element.text.strip()
+                if text:
+                    self.append(text)
+
+
+class XmlDict(dict):
+    def __init__(self, parent_element):
+        if parent_element.items():
+            self.update(dict(parent_element.items()))
+        for element in parent_element:
+            if element:
+                if len(element) == 1 or element[0].tag != element[1].tag:
+                    aDict = XmlDict(element)
+                else:
+                    aDict = {element[0].tag: XmlList(element)}
+                if element.items():
+                    aDict.update(dict(element.items()))
+                self.update({element.tag: aDict})
+            elif element.items():
+                self.update({element.tag: dict(element.items())})
+            else:
+                self.update({element.tag: element.text})
+
+
+# USAGE:
+# from xml.etree import cElementTree as ElementTree
+# tree = ElementTree.parse(<path_to_xml_file>)
+# root = tree.getroot()
