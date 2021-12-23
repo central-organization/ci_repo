7edff24fdf56af05c6943bcb29ae2269f8349fa9
Ivan Kožul <ivan.kozul@tttech-auto.com>
2021-12-23 15:10:48 +0100

    Add additional tests

--- Tools/validate_pr_package/src/test_validate_pr_package.py
+++ Tools/validate_pr_package/src/test_validate_pr_package.py
@@ -1,7 +1,5 @@
 import unittest
 from unittest.mock import MagicMock, patch
-import sys
-sys.path.append('../src')
 from validate_pr_package import *
 
 class MockResponse():
@@ -11,75 +9,87 @@ class MockResponse():
         self.headers = {"X-Checksum-Sha256": checksum}
 
 
+class MockTreeNode():
+
+    def __init__(self, tag, text):
+        self.tag = tag
+        self.text = text
+
+
+class MockElementTreeParse():
+
+    def __init__(self, repo_name):
+        self.root = [[MockTreeNode('GitHub_repository_name', repo_name),
+                        MockTreeNode('Unofficial_Artifactory_repository_path', 'unoff'),
+                        MockTreeNode('Official_Artifactory_repository_path', 'off')]]
+
+    def getroot(self):
+        return self.root
+
+
 class MockSubprocessRun():
 
-    def __init__(self, return_code):
-        self.return_code = return_code
+    def __init__(self, cmd):
+        self.returncode = 0
+        self.stdout = 'stdout'
+        self.stderr = 'stderr'
+        self.stdout = self.stdout.encode('utf-8')
 
 
 class TestValidatePrPackage(unittest.TestCase):
 
     def test__get_provider_paths__when_master_repo_path_is_invalid__expect_error(self):
+
         with self.assertRaises(Exception):
             get_provider_paths('invalid', 'invalid')
 
-    #@patch('src.validate_pr_package.requests.get', return_value = MagicMock(MockResponse(404, 105)))
+    @patch('validate_pr_package.XmlDict', return_value = {'Unofficial_Artifactory_repository_path': 'unoff',
+                                                                     'Official_Artifactory_repository_path': 'off'})
+    @patch('validate_pr_package.ElementTree.parse', return_value = MockElementTreeParse('repo'))
+    def test__get_provider_paths__when_both_arguments_are_valid__expect_dictionary_with_paths(self, mock_parse, mock_xmldict):
+
+        actual_value = get_provider_paths('repo', 'random')
+        self.assertEqual(actual_value, {'unofficial': 'unoff', 'official': 'off'})
+
+
     def test__validate_downloaded_artifact__when_status_code_is_not_200__expect_false(self):
 
-        #Arrange
         response = MockResponse(404, 105)
-
-        #Act
         actual_value = validate_downloaded_artifact(response, 105)
-
-        #Assert
         self.assertFalse(actual_value, 'Test value is false')
+
 
     def test__validate_downloaded_artifact__when_status_code_is_200_and_checksum_matches__expect_true(self):
 
-        #Arrange
         response = MockResponse(200, 500)
-
-        #Act
         actual_value = validate_downloaded_artifact(response, 500)
-
-        #Assert
         self.assertTrue(actual_value, 'Test value is true')
+
 
     def test__validate_downloaded_artifact__when_status_code_is_200_but_checksum_does_not_match_or_vice_versa__expect_false(self):
 
-        #Arrange
         response = MockResponse(200, 500)
         response_2 = MockResponse(201, 500)
-
-        #Act
         actual_value = validate_downloaded_artifact(response, 501)
         actual_value_2 = validate_downloaded_artifact(response_2, 500)
-
-        #Assert
         self.assertFalse(actual_value, 'Test value is false')
         self.assertFalse(actual_value_2, 'Test value is false')
 
+
     def test__execute_git_diff_command__when_subprocess_return_code_is_not_0__expect_none(self):
 
-        #Arrange
         with patch('subprocess.run') as subprocess_run_mock:
             subprocess.run = MagicMock()
             subprocess.run.return_value.returncode = 5
-
-        #Act
             actual_value = execute_git_diff_command('random')
-
-        #Assert
         self.assertIsNone(actual_value, 'Test value is None')
 
-    """def test__execute_git_diff_command__when_there_is_diff__expect_none(self):
 
-        #Arrange
-        with patch("subprocess.run") as subproc_run_mock:
-            subprocess.run = MagicMock()
-            subprocess.run.return_value.returncode = 0
-    """
+    @patch('validate_pr_package.subprocess.run', return_value = MockSubprocessRun('git command'))
+    def test__execute_git_diff_command__when_there_is_diff__expect_diff(self, mock_subprocess_run):
+
+        actual_value = execute_git_diff_command('path')
+        self.assertEqual(actual_value, 'stdout')
 
 
 if __name__ == '__main__':
