b071f0ce565e996888d2125956b86c73b3d7e219
Magdalena Tomasic <magdalena.tomasic@tttech-auto.com>
2021-12-24 15:57:50 +0100

    fix structure of files

--- Tools/validate_pr_package/src/test/test_validate_pr_package.py
+++ Tools/validate_pr_package/src/test/test_validate_pr_package.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import MagicMock, patch
-import sys
-sys.path.append('../src')
+#import sys
+#sys.path.append('../src')
 from validate_pr_package import *
 
 class MockResponse():
@@ -10,6 +10,10 @@ class MockResponse():
         self.status_code = status_code
         self.headers = {"X-Checksum-Sha256": checksum}
 
+class MockRequest():
+
+    def __init__(self,get):
+        self.get=get
 
 class MockSubprocessRun():
 
@@ -19,9 +23,26 @@ class MockSubprocessRun():
 
 class TestValidatePrPackage(unittest.TestCase):
 
+    def setUp(self):
+        self.response1 = MockResponse(200,500)
+
+    def tearDown(self):
+        pass
+
     def test__get_provider_paths__when_master_repo_path_is_invalid__expect_error(self):
         with self.assertRaises(Exception):
             get_provider_paths('invalid', 'invalid')
+
+    def test__get_contents_of(self):
+        mRequest = MockRequest('http://www.home.com')
+        actual_value = get_contents_of(mRequest,'admin','admin')
+
+        # with patch('validate_pr_package.requests.get') as mocked_get:
+        #     mocked_get.return_value=True
+
+        #     get_contents_o = mocked_get.get_contents_of()
+        #     mocked_get.assert_called_with("www.home.com")
+        #     self.assertEqual(get_contents_o,'True')
 
     #@patch('src.validate_pr_package.requests.get', return_value = MagicMock(MockResponse(404, 105)))
     def test__validate_downloaded_artifact__when_status_code_is_not_200__expect_false(self):
@@ -38,10 +59,9 @@ class TestValidatePrPackage(unittest.TestCase):
     def test__validate_downloaded_artifact__when_status_code_is_200_and_checksum_matches__expect_true(self):
 
         #Arrange
-        response = MockResponse(200, 500)
 
         #Act
-        actual_value = validate_downloaded_artifact(response, 500)
+        actual_value = validate_downloaded_artifact(self.response1, 500)
 
         #Assert
         self.assertTrue(actual_value, 'Test value is true')
@@ -49,11 +69,10 @@ class TestValidatePrPackage(unittest.TestCase):
     def test__validate_downloaded_artifact__when_status_code_is_200_but_checksum_does_not_match_or_vice_versa__expect_false(self):
 
         #Arrange
-        response = MockResponse(200, 500)
         response_2 = MockResponse(201, 500)
 
         #Act
-        actual_value = validate_downloaded_artifact(response, 501)
+        actual_value = validate_downloaded_artifact(self.response1, 501)
         actual_value_2 = validate_downloaded_artifact(response_2, 500)
 
         #Assert
